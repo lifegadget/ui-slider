ac8836e86c039033490582810a459d936b746ea6
simon <ihmig@kaliber5.de>
2016-10-05 16:52:08 +0200

    Add options to disable importing CSS

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 /* jshint node: true */
 'use strict';
 
+var util = require('util');
+var extend = util._extend;
+
+var defaultOptions = {
+  importBootstrapSliderCSS: true,
+  importAddonCss: true
+};
+
 module.exports = {
   name: 'ui-slider',
   description: 'A flexible UI slider for ambitious Ember apps',
   included: function(app) {
-    let parentApp = (typeof app.import !== 'function' && app.app) ? app.app : app;
+    var parentApp = (typeof app.import !== 'function' && app.app) ? app.app : app;
+    var options = extend(defaultOptions, app.options['ui-slider']);
 
     parentApp.import('bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.js');
-    parentApp.import('bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.css');
-    parentApp.import('vendor/ui-slider/ui-slider.css');
+
+    if (options.importBootstrapSliderCSS) {
+      parentApp.import('bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.css');
+    }
+    if (options.importAddonCss) {
+      parentApp.import('vendor/ui-slider/ui-slider.css');
+    }
   }
 };
