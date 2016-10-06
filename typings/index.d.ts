bfed949db31fd7216db470c9d86cfbc92aa370cc
Ken Snyder <ken@ken.net>
2016-10-06 16:53:53 +0100

    reverted bower v of slider to address css issues

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="globals/bootstrap/index.d.ts" />
+/// <reference path="globals/chalk/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/showdown/index.d.ts" />
