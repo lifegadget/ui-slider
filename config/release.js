924bbdc918b0de9ba8ab495213b916bf374ca10b
Ken Snyder <ken@ken.net>
2017-02-10 04:32:17 +0000

    deps upgrade

--- config/release.js
+++ config/release.js
@@ -5,10 +5,10 @@
 module.exports = {
   // local: true,
   // remote: 'some_remote',
-  // annotation: "Release %@",
+  annotation: "Release %@",
   // message: "Bumped version to %@",
   // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
-  // publish: true,
+  publish: true,
   // strategy: 'date',
   // format: 'YYYY-MM-DD',
   // timezone: 'America/Los_Angeles',
