7136b6c4106059cf591fd970addbcf333282dc8b
Anton Bavykin <antonbavykin1991@gmail.com>
2018-11-30 21:29:36 +0200

    fix for fastboot

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 var util = require('util');
 var extend = util._extend;
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var fastbootTransform = require('fastboot-transform');
+
 var defaultOptions = {
   importBootstrapSliderCSS: true,
   importAddonCss: true
@@ -12,15 +16,33 @@ var defaultOptions = {
 module.exports = {
   name: 'ui-slider',
   description: 'A flexible UI slider for ambitious Ember apps',
+
+  treeForVendor(defaultTree) {
+    var browserVendorLib = fastbootTransform(
+      new Funnel('bower_components/seiyria-bootstrap-slider/dist', {
+        files: [
+          'bootstrap-slider.js'
+        ]
+      })
+    )
+
+    if (defaultTree) {
+      return new mergeTrees([defaultTree, browserVendorLib]);
+    }
+
+    return browserVendorLib
+  },
+
   included: function(app) {
     var parentApp = (typeof app.import !== 'function' && app.app) ? app.app : app;
     var options = extend(defaultOptions, app.options['ui-slider']);
 
-    parentApp.import('bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.js');
+    parentApp.import('vendor/bootstrap-slider.js');
 
     if (options.importBootstrapSliderCSS) {
       parentApp.import('bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.css');
     }
+
     if (options.importAddonCss) {
       parentApp.import('vendor/ui-slider/ui-slider.css');
     }
